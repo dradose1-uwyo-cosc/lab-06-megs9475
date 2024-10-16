40b7c1a65c12ad55fdd8958f6a8fe6a7bcebb6fa
megs9475 <mlongua@uwyo.edu>
2024-10-16 21:33:39 +0000

    finished lab

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Meghan Longua
 # UWYO COSC 1010
-# Submission Date
+# Submission Date:10/16/2024
 # Lab 06
-# Lab Section: 
+# Lab Section: 15
 # Sources, people worked with, help given to: 
 # your
 # comments
@@ -79,6 +79,18 @@ print(len(random_string)) # Print out the size for reference
     # You will  need to add the letter to the dictionary on first occurrence 
     # Then increment its corresponding count 
 
+dictionary = {}
+x = len(random_string)
+
+for i in random_string:
+    if i in dictionary:
+        dictionary[i] += 1
+    else:
+        dictionary[i] = 1
+
+for i in sorted(dictionary):
+    print(f"{i} : {dictionary[i]}")
+
 
 #Load all the elements into a dictionary
 #Will need to first declare a dictionary 
@@ -88,8 +100,10 @@ print(len(random_string)) # Print out the size for reference
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+most_occurred = max(dictionary, key=dictionary.get)
+least_occurred = min(dictionary, key=dictionary.get)
+
+
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
@@ -98,3 +112,6 @@ print(f"The letter that occurred the most is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+for i in sorted(dictionary):
+    print(f"{i} : {dictionary[i]/2500*100}%")
